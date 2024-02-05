338f9a57daeb30483bfd1d7183a6333e1c26f318
Leandro Rocha <leandro.aerolito@gmail.com>
2024-02-05 09:41:30 -0300

    centralizando-imagens

--- src/pages/bio.jsx
+++ src/pages/bio.jsx
@@ -14,48 +14,92 @@ export default function Bio() {
         <h2>Quem sou</h2>
         <Container>
           <h3>Um laço eterno com a música</h3>
-            <p>Seja bem-vindo ao universo sonoro de <b>Clodoaldo Mendonça</b>, um talentoso artista que há anos ilumina as noites com sua paixão pela música. Com uma carreira enraizada nas melodias e harmonias que permeiam os diferentes estilos, Mendonça conquistou seu espaço como um dos músicos mais experientes da cena noturna.</p>
-            <p>Com uma <b>habilidade única para se conectar com a audiência</b>, este incrível multiinstrumentista transforma cada apresentação em uma jornada musical envolvente. Sua trajetória no cenário noturno é marcada por uma rica diversidade de eventos, desde apresentações intimistas em bares acolhedores até performances arrebatadoras em renomados espaços culturais.</p>
-
+          <p>
+            Seja bem-vindo ao universo sonoro de <b>Clodoaldo Mendonça</b>, um
+            talentoso artista que há anos ilumina as noites com sua paixão pela
+            música. Com uma carreira enraizada nas melodias e harmonias que
+            permeiam os diferentes estilos, Mendonça conquistou seu espaço como
+            um dos músicos mais experientes da cena noturna.
+          </p>
+          <p>
+            Com uma <b>habilidade única para se conectar com a audiência</b>,
+            este incrível multiinstrumentista transforma cada apresentação em
+            uma jornada musical envolvente. Sua trajetória no cenário noturno é
+            marcada por uma rica diversidade de eventos, desde apresentações
+            intimistas em bares acolhedores até performances arrebatadoras em
+            renomados espaços culturais.
+          </p>
+          <div className="centralizaAE">
             <div className="fotoMusico">
-                <img src="/imagens/musico8.png" alt="Músico sorrindo com um violão no colo" />
+              <img
+                src="/imagens/musico8.png"
+                alt="Músico sorrindo com um violão no colo"
+              />
             </div>
+          </div>
+          <p>
+            O repertório desta fera musical vai além das interpretações
+            magistrais de clássicos consagrados; ele também se destaca como{" "}
+            <b>compositor</b>, apresentando suas próprias criações originais.
+            Suas músicas autorais refletem a profundidade de suas experiências e
+            a riqueza de suas influências musicais, criando um diálogo único
+            entre o artista e seu público.
+          </p>
 
-            <p>O repertório desta fera musical vai além das interpretações magistrais de clássicos consagrados; ele também se destaca como <b>compositor</b>, apresentando suas próprias criações originais. Suas músicas autorais refletem a profundidade de suas experiências e a riqueza de suas influências musicais, criando um diálogo único entre o artista e seu público.</p>
-        
-            <p>Ao longo dos anos, o músico cultivou uma reputação sólida não apenas pela <b>maestria técnica</b>, mas também pela <b>capacidade de transmitir emoções através de suas performances</b>. Cada acorde, cada nota, é carregada de uma narrativa que ressoa com a essência da música e transcende o tempo.</p>
-            
+          <p>
+            Ao longo dos anos, o músico cultivou uma reputação sólida não apenas
+            pela <b>maestria técnica</b>, mas também pela{" "}
+            <b>capacidade de transmitir emoções através de suas performances</b>
+            . Cada acorde, cada nota, é carregada de uma narrativa que ressoa
+            com a essência da música e transcende o tempo.
+          </p>
+          <div className="centralizaAE">
             <div className="fotoMusico">
-                <img src="/imagens/musico9.png" alt="Músico sorrindo com um violão no colo" />
+              <img
+                src="/imagens/musico9.png"
+                alt="Músico sorrindo com um violão no colo"
+              />
             </div>
-            
-            <p>Para aqueles que buscam uma  <b>experiência musical autêntica e memorável</b>, Clodoaldo Mendonça é a escolha certa. Sua paixão duradoura pela música e sua dedicação incansável em proporcionar momentos inesquecíveis fazem dele um verdadeiro ícone na noite musical.</p>
-            
-            <p>Prepare-se para se perder na magia das notas e se deixar levar pelas histórias musicais do talentoso <b>Clodoaldo Mendonça</b>, onde cada acorde é um capítulo e cada melodia é uma página virada na vasta biografia de sua carreira musical.</p>
-            <p>E como ele mesmo diz: <b>"Corda tem que vibrar!"</b>.</p>
-          </Container>
-          </StyledBio>
+          </div>
+          <p>
+            Para aqueles que buscam uma{" "}
+            <b>experiência musical autêntica e memorável</b>, Clodoaldo Mendonça
+            é a escolha certa. Sua paixão duradoura pela música e sua dedicação
+            incansável em proporcionar momentos inesquecíveis fazem dele um
+            verdadeiro ícone na noite musical.
+          </p>
+
+          <p>
+            Prepare-se para se perder na magia das notas e se deixar levar pelas
+            histórias musicais do talentoso <b>Clodoaldo Mendonça</b>, onde cada
+            acorde é um capítulo e cada melodia é uma página virada na vasta
+            biografia de sua carreira musical.
+          </p>
+          <p>
+            E como ele mesmo diz: <b>"Corda tem que vibrar!"</b>.
+          </p>
+        </Container>
+      </StyledBio>
     </>
   );
 }
 
 const StyledBio = styled.section`
   h2::before {
-      content:"😎";
+    content: "😎";
   }
 
   h3 {
     color: #8103bb;
   }
-  
+  .centralizaAE {
+    display: flex;
+    justify-content: center;
+  }
   .fotoMusico {
     max-width: min-content;
     max-height: auto;
     box-sizing: content-box;
-    align-items: center;
     border-radius: 50%;
   }
-  
-    
 `;
-
--- src/pages/galeria.jsx
+++ src/pages/galeria.jsx
@@ -19,56 +19,96 @@ export default function Galeria() {
         <Container>
           <div className="formatos">
             <article>
-            <h3>Street Music</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem doloremque possimus nemo tempore voluptatibus accusamus eius ducimus ullam voluptatem error.</p>
-            <div>
-              <img src="/imagens/musico3.png" alt="Músico sentado no amplificador tocando um violão" />
-            </div>
+              <h3>Street Music</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+                doloremque possimus nemo tempore voluptatibus accusamus eius
+                ducimus ullam voluptatem error.
+              </p>
+              <div>
+                <img
+                  src="/imagens/musico3.png"
+                  alt="Músico sentado no amplificador tocando um violão"
+                />
+              </div>
             </article>
 
             <article>
-            <h3>Show no Teatro Ipiranga</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos aliquid veritatis nam cum suscipit perspiciatis esse incidunt autem dolorem aliquam.</p>
-            <div>
-              <img src="/imagens/musico2.png" alt="Músico tocando guitarra no palco de um teatro" />
-            </div>
+              <h3>Show no Teatro Ipiranga</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+                aliquid veritatis nam cum suscipit perspiciatis esse incidunt
+                autem dolorem aliquam.
+              </p>
+              <div>
+                <img
+                  src="/imagens/musico2.png"
+                  alt="Músico tocando guitarra no palco de um teatro"
+                />
+              </div>
             </article>
 
             <article>
-            <h3>Faixas autorais</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas necessitatibus, sunt impedit maiores eaque laborum accusantium obcaecati veritatis possimus qui.</p>
-            <div>
-              <img src="/imagens/musico4.png" alt="Músico cantando com seu violão" />
-            </div>
+              <h3>Faixas autorais</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+                necessitatibus, sunt impedit maiores eaque laborum accusantium
+                obcaecati veritatis possimus qui.
+              </p>
+              <div>
+                <img
+                  src="/imagens/musico4.png"
+                  alt="Músico cantando com seu violão"
+                />
+              </div>
             </article>
 
             <article>
               <h3>Voz e Violão</h3>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos nisi minima sit maiores incidunt a dolorum, iure nemo architecto enim.</p>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos
+                nisi minima sit maiores incidunt a dolorum, iure nemo architecto
+                enim.
+              </p>
               <div>
-                <img src="/imagens/musico5.png" alt="Músico sentado num banco tocando violão" />
+                <img
+                  src="/imagens/musico5.png"
+                  alt="Músico sentado num banco tocando violão"
+                />
               </div>
             </article>
 
             <article>
               <h3>Banda Trio</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et vero sint eligendi molestiae sequi animi maxime nemo voluptas iure reprehenderit.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et vero
+                sint eligendi molestiae sequi animi maxime nemo voluptas iure
+                reprehenderit.
+              </p>
               <div>
-                <img src="/imagens/musico6.png" alt="Trio power com seus instrumentos" />
+                <img
+                  src="/imagens/musico6.png"
+                  alt="Trio power com seus instrumentos"
+                />
               </div>
             </article>
 
             <article>
               <h3>Super Banda</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci illum perspiciatis magnam unde neque ab voluptates nisi expedita culpa aliquid.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Adipisci illum perspiciatis magnam unde neque ab voluptates nisi
+                expedita culpa aliquid.
+              </p>
               <div>
-                <img src="/imagens/musico7.png" alt="Músicos tocando em um estúdio" />
+                <img
+                  src="/imagens/musico7.png"
+                  alt="Músicos tocando em um estúdio"
+                />
               </div>
             </article>
           </div>
-
         </Container>
-
       </StyledGaleria>
     </>
   );
@@ -77,34 +117,32 @@ export default function Galeria() {
 const StyledGaleria = styled.section`
   h2::before {
     content: "🎵 ";
-}
-
-article {
-  padding: 1rem;
-  max-width: 100%;
-}
-
-h3 {
-  color: #8103bb;
-}
-.formatos {
-  max-width: 100%;
- 
-}
-
-
-@media screen and (min-width: 800px) {
-  div {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    align-items: stretch
-    flex-wrap: wrap;
-    max-width:100%;
-    border-top-right-radius: var(--borda-arredondada);
-    border-bottom-right-radius: var(--borda-arredondada);
-    border-top-left-radius: var(--borda-arredondada);
-    border-bottom-left-radius: var(--borda-arredondada);  
   }
-}
+
+  article {
+    padding: 1rem;
+    max-width: 100%;
+  }
+
+  h3 {
+    color: #8103bb;
+  }
+  .formatos {
+    max-width: 100%;
+  }
+
+  @media screen and (min-width: 800px) {
+    div {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
+      align-items: stretch;
+      flex-wrap: wrap;
+      max-width: 100%;
+      border-top-right-radius: var(--borda-arredondada);
+      border-bottom-right-radius: var(--borda-arredondada);
+      border-top-left-radius: var(--borda-arredondada);
+      border-bottom-left-radius: var(--borda-arredondada);
+    }
+  }
 `;
